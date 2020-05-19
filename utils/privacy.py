f662038abbc4578715dec7d18ee728a89de4e429
Andrew Enger <andrew.enger48@gmail.com>
2020-05-19 14:36:41 -0400

    privacy fixed :)

--- utils/privacy.py
+++ utils/privacy.py
@@ -2,6 +2,7 @@ import requests
 from utils.card import Card
 from pprint import pprint
 
+
 class PrivacySession:
     sessionid = ""
     token = ""
@@ -13,93 +14,106 @@ class PrivacySession:
         self.token = self.login(self.sessionid, username, password)
 
     def prelogin(self):
-    	headers = {
-    		'Accept': 'application/json, text/plain, */*',
-    		'Accept-Encoding': 'gzip, deflate, br',
-    		'Accept-Language': 'en-US,en;q=0.9',
-    		'Connection': 'keep-alive',
-    		'Content-Type': 'application/json;charset=UTF-8',
-    		'Host': 'privacy.com',
-    		'Origin': 'https://privacy.com',
-    		'Referer': 'https://privacy.com/login',
-    		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
-    	}
-    	print("Starting pre-login...")
-    	r = requests.get('https://privacy.com/login', headers=headers)
-    	print("Got pre-login response")
-    	if (r.status_code == 200):
-    		try:
-    			print("Got sessionID header")
-    			return r.headers['set-cookie'].split("; ")[0].replace('sessionID=', '')
-    		except:
-    			print("Error getting pre-login sessionID")
-    	else:
-    		print('Bad response when getting pre-login - ' + r.text)
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Host': 'privacy.com',
+            'Origin': 'https://privacy.com',
+            'Referer': 'https://privacy.com/login',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+        }
+        print("Starting pre-login...")
+        r = requests.get('https://privacy.com/login', headers=headers)
+        print("Got pre-login response")
+        if (r.status_code == 200):
+            try:
+                print("Got sessionID header")
+                return r.headers['set-cookie'].split("; ")[0].replace('sessionID=', '')
+            except:
+                print("Error getting pre-login sessionID")
+        else:
+            print('Bad response when getting pre-login - ' + r.text)
 
     def login(self, sessionid, username, password):
-    	cookies = {
-    		'ETag':'"ps26i5unssI="',
-    		'sessionID':sessionid
-    	}
-    	headers = {
-    		'Origin': 'https://privacy.com',
-    		'Accept-Encoding': 'gzip, deflate, br',
-    		'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
-    		'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
-    		'Content-Type': 'application/json;charset=UTF-8',
-    		'Accept': 'application/json, text/plain, */*',
-    		'Referer': 'https://privacy.com/login',
-    		'Connection': 'keep-alive',
-    		'DNT': '1',
-    	}
-    	data = {
-    		'email':username,
-    		'password':password
-    	}
-    	print("Starting login...")
-    	r = requests.post('https://privacy.com/auth/local', headers=headers, cookies=cookies, json=data)
-    	print("Got login response")
-    	if (r.status_code == 200):
-    		try:
-    			body = r.json()
-    			return body['token']
-    		except Exception as e:
-    			print("Error on login")
-    			print(e)
-    	else:
-    		print(str(r.status_code) + ': Bad response when posting login form - ' + r.text)
+        cookies = {
+            'ETag': '"ps26i5unssI="',
+            'sessionID': sessionid
+        }
+        headers = {
+            'Origin': 'https://privacy.com',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
+            'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Accept': 'application/json, text/plain, */*',
+            'Referer': 'https://privacy.com/login',
+            'Connection': 'keep-alive',
+            'DNT': '1',
+        }
+        data = {
+            'email': username,
+            'password': password
+        }
+        print("Starting login...")
+        r = requests.post('https://privacy.com/auth/local',
+                          headers=headers, cookies=cookies, json=data)
+        print("Got login response")
+        if (r.status_code == 200):
+            try:
+                body = r.json()
+                if ('oneTimeCode' in body):
+                    code = input("Please enter code sent to your email: ")
+                    codedata = {
+                        "code":code,
+                        "userToken":body['userToken'],
+                        "rememberDevice": True
+                    }
+                    r_code = requests.post('https://privacy.com/auth/local/code', headers=headers, cookies=cookies, json=codedata)
+                    if (r_code.status_code == 200):
+                        return r_code.json()['token']
+                else:
+                    return body['token']
+            except Exception as e:
+                print("Error on login")
+                print(e)
+        else:
+            print(str(r.status_code) +
+                  ': Bad response when posting login form - ' + r.text)
 
     def getTransactions(self):
-    	cookies = {
-    		'sessionID':self.sessionid,
-    		'token':self.token,
-    		'ETag':'"ps26i5unssI="'
-    	}
-    	headers = {
-    		'Accept': 'application/json, text/plain, */*',
-    		'Accept-Encoding': 'gzip, deflate, br',
-    		'Accept-Language': 'en-US,en;q=0.9',
-    		'Authorization': 'Bearer ' + self.token,
-    		'Cache-Control': 'no-cache',
-    		'Connection': 'keep-alive',
-    		'Content-Type': 'application/json;charset=UTF-8',
-    		'DNT': '1',
-    		'Host': 'privacy.com',
-    		'Origin': 'https://privacy.com',
-    		'Referer': 'https://privacy.com/home',
-    		'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
-    	}
-    	print('Getting transactions...')
-    	r = requests.get('https://privacy.com/api/v1/transaction',cookies=cookies, headers=headers)
-    	print('Got transaction response')
-    	if (r.status_code == 200):
-    		try:
-    			return r.json()
-    		except:
-    			print("Error getting transactions")
-    	else:
-    		print('Bad response when getting transactions with code - ' + r.text)
-
+        cookies = {
+            'sessionID': self.sessionid,
+            'token': self.token,
+            'ETag': '"ps26i5unssI="'
+        }
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Authorization': 'Bearer ' + self.token,
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'DNT': '1',
+            'Host': 'privacy.com',
+            'Origin': 'https://privacy.com',
+            'Referer': 'https://privacy.com/home',
+            'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
+        }
+        print('Getting transactions...')
+        r = requests.get('https://privacy.com/api/v1/transaction',
+                         cookies=cookies, headers=headers)
+        print('Got transaction response')
+        if (r.status_code == 200):
+            try:
+                return r.json()
+            except:
+                print("Error getting transactions")
+        else:
+            print('Bad response when getting transactions with code - ' + r.text)
 
     def findNewCards(self):
         cards = self.getCards()
@@ -117,39 +131,40 @@ class PrivacySession:
                 newcards.append(card)
         return newcards
 
-
     def getCards(self):
-    	cookies = {
-    		'sessionID':self.sessionid,
-    		'token':self.token,
-    		'ETag':'"ps26i5unssI="'
-    	}
-    	headers = {
-    		'Accept': 'application/json, text/plain, */*',
-    		'Accept-Encoding': 'gzip, deflate, br',
-    		'Accept-Language': 'en-US,en;q=0.9',
-    		'Authorization': 'Bearer ' + self.token,
-    		'Cache-Control': 'no-cache',
-    		'Connection': 'keep-alive',
-    		'Content-Type': 'application/json;charset=UTF-8',
-    		'DNT': '1',
-    		'Host': 'privacy.com',
-    		'Origin': 'https://privacy.com',
-    		'Referer': 'https://privacy.com/home',
-    		'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
-    	}
-    	print('Getting cards...')
-    	r = requests.get('https://privacy.com/api/v1/card',cookies=cookies, headers=headers)
-    	print('Got card response')
-    	if (r.status_code == 200):
+        cookies = {
+            'sessionID': self.sessionid,
+            'token': self.token,
+            'ETag': '"ps26i5unssI="'
+        }
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Authorization': 'Bearer ' + self.token,
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'DNT': '1',
+            'Host': 'privacy.com',
+            'Origin': 'https://privacy.com',
+            'Referer': 'https://privacy.com/home',
+            'User-Agent': 'privacy-app/2.11.0.3 iOS/12.0',
+        }
+        print('Getting cards...')
+        r = requests.get('https://privacy.com/api/v1/card',
+                         cookies=cookies, headers=headers)
+        print('Got card response')
+        if (r.status_code == 200):
             try:
                 cardlist = []
                 for card in r.json()['cardList']:
                     if card['state'] == "OPEN":
-                        cardlist.append(Card(card['cardID'], "Visa", card['PAN'], card['CVV'], card['expMonth'], card['expYear'], card['unused']))
+                        cardlist.append(Card(
+                            card['cardID'], "Visa", card['PAN'], card['CVV'], card['expMonth'], card['expYear'], card['unused']))
                 return cardlist
             except Exception as e:
                 print(e)
                 print("Error getting cards")
-    	else:
-    		print('Bad response when getting cards with code - ' + r.text)
+        else:
+            print('Bad response when getting cards with code - ' + r.text)
